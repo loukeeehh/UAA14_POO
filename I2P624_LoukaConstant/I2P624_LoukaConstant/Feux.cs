195786fca887cf916299bf54107dd6f8a04c4fe9
loukeeehh <louka.constant@gmail.com>
2024-10-15 11:37:48 +0200

    I2P624_LoukaConstant

---
+++ I2P624_LoukaConstant/I2P624_LoukaConstant/Feux.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace I2P624_LoukaConstant
+{
+    internal class Feux
+    {
+        int _couleur;
+        string _identifiant;
+        bool _etat;
+    }
+
+    public bool Etat(bool etat)
+    {
+        get { return _etat; }
+        set { _etat = value; }
+    }
+
+    
+    public int ChangeCouleur()
+    {
+        
+    }
+
+    public bool ChangeEtat(bool Allume)
+    {
+        Allume = true;
+
+        if (Allume)
+        {
+            Console.WriteLine("Le feu de signalisation est allumé");
+        }
+
+        else
+        {
+            Console.WriteLine("Le feu de signalisation est éteint");
+        }
+
+    }
+
+    public void Clignote(int nbAlternance)
+    {
+        bool Allume = true;
+
+        for (int i = 0; i < nbAlternance + 1; i++)
+        {
+            if (Allume)
+            {
+                Console.WriteLine("Le feu de signalisation est allumé");
+            }    
+        }
+
+
+    }
+
+    public string AfficheCouleur(int couleur)
+    {
+
+    }
+
+    public string AfficheEtat()
+    {
+
+    }
+
+
+
+}
---
+++ I2P624_LoukaConstant/I2P624_LoukaConstant/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace I2P624_LoukaConstant
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Etats et couleurs des feux :");
+            Console.WriteLine("----------------------------\n");
+            Feux mesFeux = new Feux();
+            int nbreFeux1 = 1001;
+            int nbreFeux2 = 007;
+            int nbAlternance = 5;
+            int[] Feux = new int[2];
+
+            Console.WriteLine("Le feu de signalisation " + nbreFeux1 + " est ");
+            Console.WriteLine("Le feu de signalisation " + nbreFeux2 + " est \n");
+
+            Console.WriteLine("Faire passer le " + nbreFeux2 + " à l'orange :");
+            Console.WriteLine("----------------------------------\n");
+
+
+            Console.WriteLine("Feu clignotant : ");
+            Console.WriteLine("----------------------------\n");
+
+        }
+    }
+}
