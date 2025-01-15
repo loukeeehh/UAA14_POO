b906a574cbf50d18ff4186b5bd05694562d1d21f
loukeeehh <louka.constant@gmail.com>
2025-01-15 22:36:02 +0100

    ACT 10

---
+++ 6T25_LoukaConstant_ACT10/6T25_LoukaConstant_ACT10/Bibliotheque.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Permissions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6T25_LoukaConstant_ACT10
+{
+    internal class Bibliotheque
+    {
+        private List<Livre> _livres;
+
+		public List<Livre> Livres
+		{
+			get { return _livres; }
+		}
+
+		public Bibliotheque(List<Livre> livres)
+		{
+			_livres = livres;
+		}
+
+		public void Ajoute(Livre livre)
+		{
+			_livres.Add(livre);
+		}
+
+		public void SupprimeLivresAbimes() // supprimer dans une liste
+		{
+			for(int i = 0; i < _livres.Count; i++)
+			{
+				if (_livres[i].Etat == 0) // Livres à tel placement dont l'état vaut 0
+				{
+                    _livres.RemoveAt(i);
+                }
+				
+			}
+		}
+
+		public string Inventaire()
+		{
+			string infos = "";
+			for (int i = 0; i < _livres.Count; i++)
+			{
+				infos += Livres[i].Auteur + " " + Livres[i].Titre + " " + Livres[i].Etat + "\n";
+			}
+
+			return infos;
+		}
+
+	}
+
+}
+
+
---
+++ 6T25_LoukaConstant_ACT10/6T25_LoukaConstant_ACT10/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace _6T25_LoukaConstant_ACT10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bienvenue à la bibliothèque !");
+            Livre livre = new Livre("Le Hobbit", "Tolkien", 3);
+        }
+    }
+}
